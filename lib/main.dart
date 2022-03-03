562a233c760a290090e2462f10fbc6cd240c74f7
sandervuurstaek <sander.vuurstaek@student.kuleuven.be>
2022-03-03 11:50:21 +0100

    Login button works

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:plantbuddy/controller/auth_manager.dart';
 import 'package:plantbuddy/views/home.dart';
 import 'package:plantbuddy/ui/startpage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Main());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
 
   MaterialColor generateMaterialColor(Color color) {
     return MaterialColor(color.value, {
@@ -47,30 +48,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Plant Buddy',
       theme: ThemeData(
+        androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
         primarySwatch:
             generateMaterialColor(const Color.fromRGBO(18, 144, 122, 1.0)),
       ),
-      home: const MyHomePage(title: 'Plant buddy'),
+      home: ValueListenableBuilder(
+        valueListenable: AuthManager.loggedInNotifier,
+        builder: (context, bool loggedIn, _) =>
+            loggedIn ? const Home() : const Frontpage(),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool loggedIn = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return loggedIn ? const Home() : const Frontpage();
   }
 }
--- lib/ui/loginpage.dart
+++ lib/ui/loginpage.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:plantbuddy/controller/auth_manager.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -11,9 +11,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final FocusNode _focusNodeUserName =  FocusNode();
-  final FocusNode _focusNodePassword =  FocusNode();
-  final TextEditingController _userNameController= TextEditingController();
+  final FocusNode _focusNodeUserName = FocusNode();
+  final FocusNode _focusNodePassword = FocusNode();
+  final TextEditingController _userNameController = TextEditingController();
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
@@ -23,242 +23,234 @@ class _LoginState extends State<Login> {
   var _isShowClear = false;
 
   @override
-  void initState(){
+  void initState() {
     _focusNodeUserName.addListener(_focusNodeListener);
     _focusNodePassword.addListener(_focusNodeListener);
 
     _userNameController.addListener(() {
       print(_userNameController.text);
-      if(_userNameController.text.length>0){
+      if (_userNameController.text.length > 0) {
         _isShowClear = true;
+      } else {
+        _isShowClear = false;
       }
-      else{
-        _isShowClear =false;
-      }
-      setState(() {
-
-      });
+      setState(() {});
     });
     super.initState();
   }
+
   @override
-  void dispose(){
+  void dispose() {
     _focusNodeUserName.removeListener(_focusNodeListener);
     _focusNodePassword.removeListener(_focusNodeListener);
     _userNameController.dispose();
     super.dispose();
   }
 
-  Future<Null> _focusNodeListener()async{
-    if(_focusNodeUserName.hasFocus){
+  Future<Null> _focusNodeListener() async {
+    if (_focusNodeUserName.hasFocus) {
       print("UserName get focus");
       _focusNodePassword.unfocus();
     }
-    if(_focusNodePassword.hasFocus){
+    if (_focusNodePassword.hasFocus) {
       print("Password get focus");
       _focusNodeUserName.unfocus();
     }
   }
 
   Widget build(BuildContext context) {
+    Widget logoImageArea = Container(
+      margin: EdgeInsets.only(left: 60.w, right: 60.w),
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: Image.asset(
+        "asset/loginpage.png",
+        height: 600.h,
+      ),
+    );
 
-     Widget logoImageArea = Container(
-       margin: EdgeInsets.only(left: 60.w,right: 60.w),
-       width: double.infinity,
-       alignment: Alignment.center,
-       child:  Image.asset("asset/loginpage.png",
-         height: 600.h,
-       ),
-     );
+    Widget inputTextArea = Container(
+      margin: EdgeInsets.only(left: 60.w, right: 60.w),
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: Colors.white),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _userNameController,
+              focusNode: _focusNodeUserName,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                    borderSide: BorderSide(color: Colors.grey, width: 6.0.r)),
+                labelText: "Username",
+                hintText: "phone number/email",
+                prefixIcon: const Icon(Icons.person),
+                suffixIcon: (_isShowClear)
+                    ? IconButton(
+                        onPressed: () {
+                          _userNameController.clear();
+                        },
+                        icon: const Icon(Icons.clear))
+                    : null,
+              ),
+              onSaved: (value) {
+                _username = value;
+              },
+            ),
+            SizedBox(height: 30.h),
+            TextFormField(
+              focusNode: _focusNodePassword,
+              obscureText: _passwordVisible,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30.r)),
+                      borderSide: BorderSide(color: Colors.grey, width: 6.0.r)),
+                  labelText: "Password",
+                  hintText: "Please enter password",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon((_passwordVisible)
+                        ? Icons.visibility
+                        : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  )),
+              onSaved: (value) {
+                _password = value;
+              },
+            )
+          ],
+        ),
+      ),
+    );
 
-     Widget inputTextArea = Container(
-       margin:  EdgeInsets.only(left: 60.w,right: 60.w),
-       decoration: const BoxDecoration(
-         borderRadius: BorderRadius.all(Radius.circular(8)),
-         color: Colors.white
-       ),
-       child: Form(
-         key: _formKey,
-         child: Column(
-           mainAxisSize: MainAxisSize.min,
-           children: [
-             TextFormField(
-               controller: _userNameController,
-               focusNode: _focusNodeUserName,
-               decoration: InputDecoration(
-                 border:  OutlineInputBorder(
-                   borderRadius: BorderRadius.all( Radius.circular(30.r)),
-                   borderSide: BorderSide(
-                     color: Colors.grey,
-                     width: 6.0.r
-                   )
-                 ),
-                 labelText: "Username",
-                 hintText: "phone number/email",
-                 prefixIcon: const Icon(Icons.person),
-                 suffixIcon: (_isShowClear)? IconButton(onPressed: (){
-                   _userNameController.clear();
-                 }, icon: const Icon(Icons.clear)):null,
-               ),
-               onSaved: (value){
-                 _username=value;
-               },
-             ),
-              SizedBox(height: 30.h),
-             TextFormField(
-               focusNode: _focusNodePassword,
-               obscureText: _passwordVisible,
-               decoration: InputDecoration(
-                   border: OutlineInputBorder(
-                       borderRadius: BorderRadius.all( Radius.circular(30.r)),
-                       borderSide: BorderSide(
-                           color: Colors.grey,
-                           width: 6.0.r
-                       )
-                   ),
-                 labelText: "Password",
-                 hintText: "Please enter password",
+    Widget loginButtonArea = Container(
+      margin: EdgeInsets.only(left: 60.w, right: 60.w, top: 80.h),
+      height: 150.h,
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          elevation: MaterialStateProperty.all(10),
+          backgroundColor: MaterialStateProperty.all(
+              const Color.fromRGBO(18, 144, 122, 1.0)),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(100.r))),
+        ),
+        child: Text(
+          "sign in",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 60.sp),
+        ),
+        onPressed: () {
+          AuthManager.login("username", "password");
+          Navigator.pop(context);
+        },
+      ),
+    );
 
-                 prefixIcon: const Icon(Icons.lock),
-                 suffixIcon: IconButton(
-                   icon: Icon((_passwordVisible)?Icons.visibility: Icons.visibility_off),
-                   onPressed: (){
-                     setState(() {
-                       _passwordVisible=!_passwordVisible;
-                     });
-                   },
-                 )
-               ),
-               onSaved: (value){
-                 _password=value;
-               },
-               )
-           ],
-         ),
+    Widget thridLoginArea = Container(
+      margin: EdgeInsets.only(left: 60.w, right: 60.w, top: 80.h),
+      child: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 250.w,
+                height: 3.0.h,
+                color: Colors.grey,
+              ),
+              Text("Third party sign in",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
+                    fontSize: 40.sp,
+                  )),
+              Container(
+                width: 250.w,
+                height: 3.0.h,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 50.h,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(FontAwesomeIcons.weixin),
+                color: Colors.green[200],
+                iconSize: 120.r,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(FontAwesomeIcons.facebook),
+                color: Colors.green[200],
+                iconSize: 120.r,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(FontAwesomeIcons.google),
+                color: Colors.green[200],
+                iconSize: 120.r,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
 
-
-       ),
-     );
-
-     Widget loginButtonArea = Container(
-       margin:  EdgeInsets.only(left: 60.w,right: 60.w, top:80.h),
-       height: 150.h,
-       width: double.infinity,
-       child: ElevatedButton(
-         style: ButtonStyle(
-           elevation: MaterialStateProperty.all(10),
-           backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(
-               18, 144, 122, 1.0)),
-           shape: MaterialStateProperty.all(RoundedRectangleBorder(
-               borderRadius: BorderRadius.circular(100.r))),
-         ),
-         child: Text("sign in",
-           style: TextStyle(
-               fontWeight: FontWeight.bold,
-               color: Colors.white,
-               fontSize: 60.sp
-           ),
-         ),
-         onPressed: (){
-         },
-       ),
-     );
-
-     Widget thridLoginArea = Container(
-       margin:  EdgeInsets.only(left: 60.w,right: 60.w, top:80.h),
-       child: Column(
-         children: [
-           Row(
-             mainAxisSize: MainAxisSize.max,
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-               Container(
-                 width: 250.w,
-                 height: 3.0.h,
-                 color: Colors.grey,
-               ),
-               Text("Third party sign in",
-                 style: TextStyle(fontWeight: FontWeight.bold,
-                 color: Colors.grey,
-                 fontSize: 40.sp,
-                 )
-               ),
-               Container(
-                 width: 250.w,
-                 height: 3.0.h,
-                 color: Colors.grey,
-               ),
-             ],
-           ),
-            SizedBox(
-             height: 50.h,
-           ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-             children: [
-               IconButton(
-                   onPressed: (){
-               },
-                   icon: const Icon(FontAwesomeIcons.weixin),
-                 color: Colors.green[200],
-                 iconSize: 120.r,
-               ),
-               IconButton(
-                 onPressed: (){
-                 },
-                 icon: const Icon(FontAwesomeIcons.facebook),
-                 color: Colors.green[200],
-                 iconSize: 120.r,
-               ),
-               IconButton(
-                 onPressed: (){
-                 },
-                 icon: const Icon(FontAwesomeIcons.google),
-                 color: Colors.green[200],
-                 iconSize: 120.r,
-               ),
-             ],
-           )
-         ],
-       ),
-     );
-
-     Widget bottomArea = Container(
-       margin: EdgeInsets.only(left: 60.w,right: 60.w, top: 60.h),
-       child:
-           TextButton(onPressed: (){}, child: Text(
-             "forget password?",
-             style: TextStyle(
-               color: Colors.blue[400],
-               fontSize: 45.0.sp,
-             ),
-           )
-       ),
-     );
+    Widget bottomArea = Container(
+      margin: EdgeInsets.only(left: 60.w, right: 60.w, top: 60.h),
+      child: TextButton(
+          onPressed: () {},
+          child: Text(
+            "forget password?",
+            style: TextStyle(
+              color: Colors.blue[400],
+              fontSize: 45.0.sp,
+            ),
+          )),
+    );
 
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black,),
-          onPressed: ()=>Navigator.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () => Navigator.of(context).pop(),
         ),
         backgroundColor: Colors.white,
       ),
       body: GestureDetector(
-        onTap: (){
+        onTap: () {
           _focusNodeUserName.unfocus();
           _focusNodePassword.unfocus();
         },
-        child: ListView(
-          children:[logoImageArea,
-            inputTextArea,
-            loginButtonArea,
-            thridLoginArea,
-            bottomArea]
-          ),
+        child: ListView(children: [
+          logoImageArea,
+          inputTextArea,
+          loginButtonArea,
+          thridLoginArea,
+          bottomArea
+        ]),
       ),
-
     );
   }
 }
